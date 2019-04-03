b17de5f3c722087b8b78a1c52d84f656e41e32fa
Christian Anderson <ca96187@gmail.com>
2019-04-03 17:37:12 -0600

    still needs css

---
+++ src/components/Next.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Next(props) {
+  return (
+    <button onClick={props.task}>Next</button>
+  )
+}
---
+++ src/components/Previous.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Previous(props) {
+  return (
+    <button onClick={props.task}>Previous</button>
+  )
+}
